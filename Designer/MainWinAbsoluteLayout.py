9c6b8ea95790c02a146cb53c9f769f875eef31e9
Johnson Tsai <twjohnsontsai@icloud.com>
2020-02-27 01:57:09 +0800

    绝对布局

---
+++ Designer/MainWinAbsoluteLayout.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'MainWinAbsoluteLayout.ui'
+#
+# Created by: PyQt5 UI code generator 5.9.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(50, 10, 181, 51))
+        self.pushButton.setObjectName("pushButton")
+        self.radioButton = QtWidgets.QRadioButton(self.centralwidget)
+        self.radioButton.setGeometry(QtCore.QRect(60, 80, 100, 20))
+        self.radioButton.setObjectName("radioButton")
+        self.checkBox = QtWidgets.QCheckBox(self.centralwidget)
+        self.checkBox.setGeometry(QtCore.QRect(190, 80, 87, 20))
+        self.checkBox.setObjectName("checkBox")
+        self.tableView = QtWidgets.QTableView(self.centralwidget)
+        self.tableView.setGeometry(QtCore.QRect(70, 200, 256, 192))
+        self.tableView.setObjectName("tableView")
+        self.timeEdit = QtWidgets.QTimeEdit(self.centralwidget)
+        self.timeEdit.setGeometry(QtCore.QRect(100, 450, 118, 24))
+        self.timeEdit.setObjectName("timeEdit")
+        self.dateEdit = QtWidgets.QDateEdit(self.centralwidget)
+        self.dateEdit.setGeometry(QtCore.QRect(240, 450, 110, 24))
+        self.dateEdit.setObjectName("dateEdit")
+        self.calendarWidget = QtWidgets.QCalendarWidget(self.centralwidget)
+        self.calendarWidget.setGeometry(QtCore.QRect(360, 0, 296, 173))
+        self.calendarWidget.setObjectName("calendarWidget")
+        self.webEngineView = QtWebEngineWidgets.QWebEngineView(self.centralwidget)
+        self.webEngineView.setGeometry(QtCore.QRect(369, 179, 381, 311))
+        self.webEngineView.setUrl(QtCore.QUrl("https://www.google.com.tw/?gws_rd=ssl"))
+        self.webEngineView.setObjectName("webEngineView")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 22))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "PushButton"))
+        self.radioButton.setText(_translate("MainWindow", "RadioButton"))
+        self.checkBox.setText(_translate("MainWindow", "CheckBox"))
+
+from PyQt5 import QtWebEngineWidgets
