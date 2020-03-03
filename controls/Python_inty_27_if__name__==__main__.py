c8e7fc6c1dd727d60916f090af2e74ad7ee9c6bb
Johnson Tsai <twjohnsontsai@icloud.com>
2020-03-03 15:33:26 +0800

    if __name__=='__main__'

---
+++ controls/Python_inty_27_if__name__==__main__.py
@@ -0,0 +1,13 @@
+import johnson
+
+# 调用了johnson模块，所以也会和下行的代码一起执行
+print(('this is another moduel'))
+
+
+def m2():
+    print('this is youtube module')
+m2()
+# johnson.py如果没有限制调用，这时候执行m2就会把johnson.py的代码也执行，
+# 如果johnson.py限定设置了if __name__=='__main__'，就不会执行了
+# 当有调用johnson.py的m1()时，他才会出现
+johnson.m1()
